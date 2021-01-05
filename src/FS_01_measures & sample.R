c45d35ed3c158713a1b62f08cf5874b095cf4ba7
Joanna R Pepin <JoannaPepin@gmail.com>
2021-01-05 12:03:16 -0500

    processed demo variables

--- src/FS_01_measures & sample.R
+++ src/FS_01_measures & sample.R
@@ -45,10 +45,10 @@ data$gender %>% attr('labels') ### Viewing value labels
 
 fcols <- c("gender", "educ", "work", "relate", "income", "race", "relfreq", "religion",
           "region", "mar", "parent", "relinc", "dur", "item", "iperson", "activity", 
-           "aperson", "ifair", "afair", "organize", "PPT01", "PPT25", "PPT612", "PPT1317",
-           "DOV_B02_MaxValue", "DOV_B03_MaxValue" )
+           "aperson", "ifair", "afair", "organize", "DOV_B02_MaxValue", "DOV_B03_MaxValue")
 
-icols <- c("age", "herjoint", "hisjoint", "herindv", "hisindv" )
+icols <- c("age", "PPT01", "PPT25", "PPT612", "PPT1317", 
+           "herjoint", "hisjoint", "herindv", "hisindv")
 
 ccols <- c("CaseID", "iqual", "aqual")
 
@@ -58,9 +58,167 @@ data[ccols] <- lapply(data[ccols], as.character)
 
 
 #####################################################################################
-# Prep the variables for analysis
+# Prep the demographic variables for analysis
 #####################################################################################
 
-## gender -------------------------------------------------------------------------
+## age -----------------------------------------------------------------------------
+summary(data$age)
 
+## gender --------------------------------------------------------------------------
 table(data$gender)
+  data$gender <-data$gender %>%
+    droplevels()
+  levels(atus$sex) <- c('Male', 'Female')
+table(data$gender)
+
+## education -----------------------------------------------------------------------
+table(data$educ)
+  data$educ <-data$educ %>%
+    droplevels()
+table(data$educ)
+
+## work ----------------------------------------------------------------------------
+table(data$work)
+  data$work <-data$work %>%
+    droplevels()
+
+  data <- data %>%
+    mutate(
+      employ = case_when(
+        work   == "Working - as a paid employee"                  | 
+        work == "Working - self-employed"                       ~ "Employed",
+        work   == "Not working - on temporary layoff from a job"  |
+        work == "Not working - looking for work"                ~ "Unemployed",
+        work   == "Not working - retired"                         |
+        work == "Not working - disabled"                          |
+        work == "Not working - other"                           ~ "Not in labor force",
+        TRUE                                                    ~  NA_character_ 
+      ))
+  
+  data$employ <- factor(data$employ, levels = c("Employed", "Unemployed", "Not in labor force"), ordered = FALSE)
+table(data$employ)
+
+## relate -------------------------------------------------------------------------
+table(data$relate)
+  data$relate <-data$relate %>%
+    droplevels()
+
+  data <- data %>%
+    mutate(
+      relate = case_when(
+        relate == "Married"                       ~ "Married",
+        relate == "Living with partner"           ~ "Cohabit",
+        relate == "Never married"                 ~ "Never married",
+        relate == "Divorced"                        |
+        relate == "Separated"                       |
+        relate == "Widowed"                       ~ "Divorced/Sep./Widow",
+        TRUE                                      ~  NA_character_ 
+      ))
+
+  data$relate <- factor(data$relate, levels = c("Married", "Cohabit", "Never married", "Divorced/Sep./Widow"), ordered = FALSE)
+table(data$relate)
+
+## income -------------------------------------------------------------------------
+table(data$income)
+  data$income <-data$income %>%
+    droplevels() %>%
+    as.integer()
+  
+  data <- data %>%
+    mutate(
+      incdum = case_when(
+        income <= 11          ~ "< than $50,000",
+        income >= 12          ~ "> than $50,000",
+        TRUE                  ~  NA_character_ 
+      ))
+  
+  data$incdum <- factor(data$incdum, levels = c("< than $50,000", "> than $50,000"), ordered = FALSE)
+table(data$incdum)
+  
+## race/eth ----------------------------------------------------------------------
+table(data$race)
+  data$race <-data$race %>%
+    droplevels()
+
+  data <- data %>%
+    mutate(
+      raceeth = case_when(
+        race == "White, Non-Hispanic"           ~ "White",
+        race == "Black, Non-Hispanic"           ~ "Black",
+        race == "Hispanic"                      ~ "Hispanic",
+        race == "Other, Non-Hispanic"              |
+        race == "2+ Races, Non-Hispanic"        ~ "Other",
+        TRUE                                    ~  NA_character_ 
+      ))
+  
+  data$raceeth <- factor(data$raceeth, levels = c("White", "Black", "Hispanic", "Other"), ordered = FALSE)
+table(data$raceeth)
+
+## religion freq ----------------------------------------------------------------
+table(data$relfreq)
+
+  data <- data %>%
+    mutate(
+      relfreq = case_when(
+        relfreq == "More than once a week"           ~ "Weekly plus",
+        relfreq == "Once a week"                     ~ "Weekly",
+        relfreq == "Once or twice a month"           ~ "Monthly",
+        relfreq == "A few times a year"              |
+        relfreq == "Once a year or less"             ~ "Yearly",
+        relfreq == "Never"                           ~ "Never",
+        relfreq == "Refused"                         ~ "Unknown",
+        TRUE                                         ~  NA_character_ 
+      ))
+
+  data$relfreq <- factor(data$relfreq, levels = c("Weekly plus", "Weekly", "Monthly", "Yearly", "Never", "Unknown"), ordered = FALSE)
+table(data$relfreq)
+
+
+## religion -------------------------------------------------------------------
+table(data$religion)
+
+  data$religion <-data$religion %>%
+    as.integer()
+  
+  data <- data %>%
+    mutate(
+      religion = case_when(
+        religion == 1          ~ "Catholic",
+        religion == 2          ~ "Evangelical or Portestant Christian",
+        religion == 13         ~ "None",
+        TRUE                   ~ "Other Religion"
+      ))
+
+  data$religion <- factor(data$religion, levels = c("Evangelical or Portestant Christian", "Catholic", 
+                                                    "None", "Other Religion"), ordered = FALSE)
+table(data$religion)
+  
+
+## region -------------------------------------------------------------------
+table(data$region)
+  data$region <-data$region %>%
+    droplevels()
+table(data$region)
+  
+
+## parent -------------------------------------------------------------------
+table(data$PPT01)
+table(data$PPT25)
+table(data$PPT612)
+table(data$PPT1317)
+
+  data <- data %>%
+    mutate(
+      parent = case_when(
+        PPT01  >= 1 |  PPT25  >= 1 | 
+        PPT612 >= 1 | PPT1317 >= 1  ~ "Parent",
+        TRUE                        ~ "Not a parent"
+      ))
+
+table(data$parent)
+
+
+#####################################################################################
+# Prep the vignette variables for analysis
+#####################################################################################
+
