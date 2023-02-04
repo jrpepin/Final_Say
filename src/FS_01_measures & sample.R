cde9ccb6dccf16b691de01f9d548e123d8611318
Joanna R Pepin <JoannaPepin@gmail.com>
2023-02-04 18:17:51 -0500

    fixing coding errors (person2

--- src/FS_01_measures & sample.R
+++ src/FS_01_measures & sample.R
@@ -20,20 +20,20 @@ data <- read_sav(file.path(dataDir, rawdata))   # load the raw data file
 data <- select(data, CaseID, weight, PPAGE, PPGENDER, PPEDUCAT, PPWORK, PPMARIT, PPINCIMP, 
                PPETHM, REL2, REL1, PPREG4, PPT01, PPT25, PPT612, PPT1317,
                DOV_RELSTAT, DOV_PARENTST, DOV_EARNINGS, DOV_RELDUR, DOV_ITEM, DOV_PERSON_B04,
-               B05, DOV_ACTIVITY, DOV_PERSON_B06, B07, B04, B06, B01, B02_Shared,
+               B05, DOV_ACTIVITY, DOV_PERSON2_B06, B07, B04, B06, B01, B02_Shared,
                B03_Shared, B02_Individual, B03_Individual, DOV_B02_MaxValue, DOV_B03_MaxValue)
 
 ## Rename variables -----------------------------------------------------------------
 data <- rename(data, 
-               age      = PPAGE,          gender    = PPGENDER,       educ       = PPEDUCAT,
-               work     = PPWORK,         relate    =	PPMARIT,        income     = PPINCIMP,
-               race     = PPETHM,         relfreq   = REL2,           religion   = REL1,
-               region   = PPREG4,         mar       = DOV_RELSTAT,  	child      = DOV_PARENTST,
-               relinc   = DOV_EARNINGS,   dur       = DOV_RELDUR,     item       = DOV_ITEM,
-               iperson  = DOV_PERSON_B04, qual1     = B05,            activity   = DOV_ACTIVITY,
-               aperson  = DOV_PERSON_B06, qual2     = B07,            ifair      = B04,
-               afair    = B06,            organize  = B01,            herjoint   = B02_Shared,
-               hisjoint = B03_Shared,     herindv   = B02_Individual, hisindv    = B03_Individual)
+               age      = PPAGE,           gender    = PPGENDER,       educ       = PPEDUCAT,
+               work     = PPWORK,          relate    =	PPMARIT,        income     = PPINCIMP,
+               race     = PPETHM,          relfreq   = REL2,           religion   = REL1,
+               region   = PPREG4,          mar       = DOV_RELSTAT,  	child      = DOV_PARENTST,
+               relinc   = DOV_EARNINGS,    dur       = DOV_RELDUR,     item       = DOV_ITEM,
+               iperson  = DOV_PERSON_B04,  qual1     = B05,            activity   = DOV_ACTIVITY,
+               aperson  = DOV_PERSON2_B06, qual2     = B07,            ifair      = B04,
+               afair    = B06,             organize  = B01,            herjoint   = B02_Shared,
+               hisjoint = B03_Shared,      herindv   = B02_Individual, hisindv    = B03_Individual)
 
 ## Generate a codebook -------------------------------------------------------------
 sjPlot::view_df(data) # Load codebook in viewer pane
